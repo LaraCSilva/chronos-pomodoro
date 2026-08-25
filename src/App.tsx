a9db9f0c508548f5333cfe75fd8fda634e3f33bc
LaraCSilva <lar1nhaszsilva@gmail.com>
2026-08-24 21:27:07 -0300

    criação do component heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import {Heading} from './components/Heading'
+
 import './styles/theme.css'
 import './styles/global.css'
-
 
 export function App() {
   return (
     <div>
-      <h1>Olá Mundo! (do APP!)</h1>
+      <Heading />
       <p>Lore ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
   );
